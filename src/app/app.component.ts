0ef3e722bb68bdc4c690b9171ddddaa00d101c36
Karthik <jeefkarthik036@gmail.com>
2022-06-08 09:09:41 +0530

    Added:Event Binding to open BridgeLabz website by clicking on image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = '../assets/images/logo.jpg';
+  url="https://www.google.com";
 
   ngOnInit():void {
     this.title = "Hello From Bridgelabz"
+  }
+
+  onClick($event:any){
+    console.log("Save button is clicked",$event);
+    window.open(this.url,"_blank");
   }
 }
