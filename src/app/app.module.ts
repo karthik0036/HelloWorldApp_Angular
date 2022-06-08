44ad41eacfc5be104e3112b1a7fc15e41f9751a6
Karthik <jeefkarthik036@gmail.com>
2022-06-08 08:40:53 +0530

    Added:One way Data Binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+// importing core modules of angular
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// whenever cli is used to generate new components services,it 
+// will automatically update this file to immport and add them here so we can see update
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
