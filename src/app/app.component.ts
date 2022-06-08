5ff68d25efcc79a1e611d79bb3f37db4c719e072
Karthik <jeefkarthik036@gmail.com>
2022-06-08 08:52:24 +0530

    Added:Property Binding to Display BridgeLabz Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl = '../assets/images/logo.jpg';
 
   ngOnInit():void {
     this.title = "Hello From Bridgelabz"
